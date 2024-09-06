5ef223a83b2a2586d59badaf5e079a6464c72c63
sossaisc <isaac-rosa2011@hotmail.com>
2024-09-06 18:26:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    //se o campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um piloto ou modalidade de corrida</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+  
+    // Intera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        //se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) ||descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // Cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+              <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <a href="${dado.link}" target="_blank" >Mais informações</a> 
+            </div>
+          `;
+        } 
+    }
+  
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado!</p>"
+    }
+
+    // Atribui o HTML construído à seção, atualizando a página com os resultados
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Felipe Drugovich",
+      descricao: "Felipe Drugovich, jovem talento do automobilismo brasileiro, tem se destacado no cenário internacional da Fórmula 1. Campeão da Fórmula 2 em 2022, o piloto maringaense chamou a atenção das principais equipes da categoria e atualmente faz parte do programa de desenvolvimento de pilotos da Aston Martin.",
+      link: "https://pt.wikipedia.org/wiki/Felipe_Drugovich",
+      tags: "maringa astor martin destaque"
+    },
+    {
+      titulo: "Sérgio Sette Câmara",
+      descricao: "Sérgio Sette Câmara é um piloto brasileiro que se destacou nas categorias de base do automobilismo. Conquistou diversos pódios e vitórias na Fórmula 2 e Fórmula 3, e chegou a ser piloto de testes e desenvolvimento da McLaren na Fórmula 1. Atualmente, compete na Fórmula E.",
+      link: "https://www.fiaformulae.com/pt-br/drivers/b768b851-5611-40be-8687-a0974e3d2754/sergio-sette-camara/biography",
+      tags: "podio mclaren boleia"
+    },
+    {
+      titulo: "Gabriel Bortoleto",
+      descricao: "Gabriel Bortoleto é um jovem piloto brasileiro que tem se destacado na Fórmula 2. Com um estilo de pilotagem agressivo e consistente, Bortoleto vem chamando a atenção das equipes e se posiciona como um dos principais nomes da nova geração do automobilismo brasileiro.",
+      link: "https://en.wikipedia.org/wiki/Gabriel_Bortoleto",
+      tags:"agressivo rapido dedicado"
+    }
+  ];
